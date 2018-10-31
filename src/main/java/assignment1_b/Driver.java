0cd583ea1caf12e99c53e26f07d11c92ae0598e7
monkfungus <s.nixon1@nuigalway.ie>
2018-10-31 19:40:38 +0000

    Complete printing of data

--- src/main/java/assignment1_b/Driver.java
+++ src/main/java/assignment1_b/Driver.java
@@ -37,7 +37,8 @@ public class Driver {
         DateTime start = new DateTime(2015, 12, 03, 0, 0);
         DateTime end = new DateTime(2019, 05, 01, 0, 0);
         Course c1 = new Course("ECE", start, end);
-        // add students to modules
+        // add students to modules and modulues to students
+        
         m1.addStudent(s1);
         m1.addStudent(s2);
         m1.addStudent(s3);
@@ -51,11 +52,22 @@ public class Driver {
         m3.addStudent(s1);
         m3.addStudent(s3);
         m3.addStudent(s5);
-        // add modules to course
-        c1.addModule(m1);
-        c1.addModule(m2);
-        c1.addModule(m3);
-        // print out a list of all data and it's relationship  
+        
+        // add modules to course and add course to students in modules
+        for (Module m : modules) {
+            c1.addModule(m);
+            for (Student s : m.getStudents()) {
+                s.addCourse(c1);
+                s.addModule(m);
+            }
+        }
+        
+        // add students to course
+        for (Student s : students) {
+            c1.addStudent(s);
+        }
+            
+        // print out a list of all data relationships
         String tab = "\t";
         String branch = "+-- ";
         System.out.println("|-- " + c1.getName());
@@ -63,6 +75,19 @@ public class Driver {
             System.out.println("|" + tab + branch + m.getName());
             for (Student s : m.getStudents()) {
                 System.out.println("|" + tab + tab + branch + s.getName());
+            }
+        }
+        
+        System.out.println();
+        for (Student s : students) {
+            System.out.println("|-- " + s.getName() + " | " + s.getUsername());
+            System.out.println("|" + tab + branch + "Courses:");
+            for (Course c : s.getCourses()) {
+                System.out.println("|" + tab + tab + branch + c.getName());
+            }
+            System.out.println("|" + tab + branch + "Modules:");
+            for (Module m : s.getModules()) {
+                System.out.println("|" + tab + tab + branch + m.getName());
             }
         }
     }   
